77ad08158792b5060f09b1e0045b5172eb206e0f
Ondrik <ondrik.bernat@gmail.com>
2018-03-01 18:14:02 +0100

    GUI formatted to pep8

--- GUI.py
+++ GUI.py
@@ -26,7 +26,11 @@ class Application(Tk):
         else:
             self.right_click = "<Button-3>"
 
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.title_font = tkfont.Font(
+            family='Helvetica',
+            size=18,
+            weight="bold",
+            slant="italic")
         self.geometry("600x500")
         self.minsize(height=500, width=600)
         self.cross_road_function()
@@ -50,29 +54,65 @@ class Application(Tk):
         self.username_entry = Entry(self)
         self.password_entry = Entry(self, show="*")
         if download:
-            self.login_button = Button(self, text="Login",
-                                       command=lambda: self.login_download(subject, self.username_entry.get(),
-                                                                           self.password_entry.get()))
+            self.login_button = Button(
+                self, text="Login", command=lambda: self.login_download(
+                    subject, self.username_entry.get(), self.password_entry.get()))
         else:
-            self.login_button = Button(self, text="Login",
-                                       command=lambda: self.login_upload(subject, self.username_entry.get(),
-                                                                         self.password_entry.get()))
+            self.login_button = Button(
+                self, text="Login", command=lambda: self.login_upload(
+                    subject, self.username_entry.get(), self.password_entry.get()))
 
-        self.back_button = Button(self, text="Back", command=self.cross_road_function)
+        self.back_button = Button(
+            self, text="Back", command=self.cross_road_function)
 
-        self.title_label.place(relx=0.42, rely=0.42, y=-120, width=120, height=25)
-        self.username_label.place(relx=0.42, x=-60, rely=0.42, y=-65, width=120, height=25)
-        self.password_label.place(relx=0.42, x=-60, rely=0.42, y=-37, width=120, height=25)
-        self.username_entry.place(relx=0.42, x=60, rely=0.42, y=-65, width=120, height=25)
-        self.password_entry.place(relx=0.42, x=60, rely=0.42, y=-37, width=120, height=25)
+        self.title_label.place(
+            relx=0.42,
+            rely=0.42,
+            y=-120,
+            width=120,
+            height=25)
+        self.username_label.place(
+            relx=0.42,
+            x=-60,
+            rely=0.42,
+            y=-65,
+            width=120,
+            height=25)
+        self.password_label.place(
+            relx=0.42,
+            x=-60,
+            rely=0.42,
+            y=-37,
+            width=120,
+            height=25)
+        self.username_entry.place(
+            relx=0.42,
+            x=60,
+            rely=0.42,
+            y=-65,
+            width=120,
+            height=25)
+        self.password_entry.place(
+            relx=0.42,
+            x=60,
+            rely=0.42,
+            y=-37,
+            width=120,
+            height=25)
         self.login_button.place(relx=0.42, rely=0.42, width=120, height=25)
-        self.back_button.place(relx=0.42, rely=0.42, y=40, width=120, height=25)
+        self.back_button.place(
+            relx=0.42,
+            rely=0.42,
+            y=40,
+            width=120,
+            height=25)
 
     def sync_page(self):
 
         self.clear_frame()
 
-        self.sync_button = Button(self, text='Sync', command=lambda: self.login_page("All"))
+        self.sync_button = Button(
+            self, text='Sync', command=lambda: self.login_page("All"))
         self.sync_button.place(relx=0.42, rely=0.42, width=120, height=25)
 
     def login_download(self, subject, name="none", password="none", ):
@@ -85,9 +125,9 @@ class Application(Tk):
         self.save_teacher(teacher)
         self.cross_road_function()
 
-    #TODO opravit aby to fungovalo s viacerymi predmetmi
-    #TODO opravit kde sa posielaju objekty a kde mena
-    #TODO upratat ten bordel
+    # TODO opravit aby to fungovalo s viacerymi predmetmi
+    # TODO opravit kde sa posielaju objekty a kde mena
+    # TODO upratat ten bordel
 
     def login_upload(self, subject_name, name="none", password="none"):
 
@@ -95,7 +135,8 @@ class Application(Tk):
         # for student in teacher.subjects_list[0].student_list:
         #     print(student.name, student.attendance )
 
-        ret_value = isp.upload_routine(teacher.subjects_list[0], name, password)
+        ret_value = isp.upload_routine(
+            teacher.subjects_list[0], name, password)
         if ret_value < 0:
             er.showError("FAIL UPLOAD")
             self.login_page(subject_name, 0)
@@ -184,10 +225,14 @@ class Application(Tk):
                     d = Button(text="Disable",
                                command=lambda text=active_subjects_list[i - 1].name: self.move_subject(text, 1))
 
-                    a.place(relx=0.375, x=-180, rely=0.20, y=(i * 30), width=120, height=25)
-                    b.place(relx=0.375, x=-60, rely=0.20, y=(i * 30), width=120, height=25)
-                    c.place(relx=0.375, x=+60, rely=0.20, y=(i * 30), width=120, height=25)
-                    d.place(relx=0.375, x=+180, rely=0.20, y=(i * 30), width=120, height=25)
+                    a.place(relx=0.375, x=-180, rely=0.20,
+                            y=(i * 30), width=120, height=25)
+                    b.place(relx=0.375, x=-60, rely=0.20,
+                            y=(i * 30), width=120, height=25)
+                    c.place(relx=0.375, x=+60, rely=0.20,
+                            y=(i * 30), width=120, height=25)
+                    d.place(relx=0.375, x=+180, rely=0.20,
+                            y=(i * 30), width=120, height=25)
 
             logout_button.place(relx=0.65, x=+114, y=0, width=75, height=25)
             sync_button.place(relx=0.65, x=+38, y=0, width=75, height=25)
@@ -224,10 +269,14 @@ class Application(Tk):
                     d = Button(tab1, text="Disable",
                                command=lambda text=active_subjects_list[i - 1].name: self.move_subject(text, 1))
 
-                    a.place(relx=0.375, x=-180, rely=0.20, y=(i * 30), width=120, height=25)
-                    b.place(relx=0.375, x=-60, rely=0.20, y=(i * 30), width=120, height=25)
-                    c.place(relx=0.375, x=+60, rely=0.20, y=(i * 30), width=120, height=25)
-                    d.place(relx=0.375, x=+180, rely=0.20, y=(i * 30), width=120, height=25)
+                    a.place(relx=0.375, x=-180, rely=0.20,
+                            y=(i * 30), width=120, height=25)
+                    b.place(relx=0.375, x=-60, rely=0.20,
+                            y=(i * 30), width=120, height=25)
+                    c.place(relx=0.375, x=+60, rely=0.20,
+                            y=(i * 30), width=120, height=25)
+                    d.place(relx=0.375, x=+180, rely=0.20,
+                            y=(i * 30), width=120, height=25)
 
             if (len(inactive_subjects_list)):
 
@@ -239,13 +288,17 @@ class Application(Tk):
                     b = Button(tab2, text="Enable",
                                command=lambda text=inactive_subjects_list[i - 1].name: self.move_subject(text, 2))
 
-                    a.place(relx=0.375, x=-60, rely=0.20, y=(i * 30), width=120, height=25)
-                    b.place(relx=0.375, x=+60, rely=0.20, y=(i * 30), width=120, height=25)
+                    a.place(relx=0.375, x=-60, rely=0.20,
+                            y=(i * 30), width=120, height=25)
+                    b.place(relx=0.375, x=+60, rely=0.20,
+                            y=(i * 30), width=120, height=25)
 
             sync_button_tab1.place(relx=0.65, x=+38, y=0, width=75, height=25)
             sync_button_tab2.place(relx=0.65, x=+38, y=0, width=75, height=25)
-            logout_button_tab1.place(relx=0.65, x=+114, y=0, width=75, height=25)
-            logout_button_tab2.place(relx=0.65, x=+114, y=0, width=75, height=25)
+            logout_button_tab1.place(
+                relx=0.65, x=+114, y=0, width=75, height=25)
+            logout_button_tab2.place(
+                relx=0.65, x=+114, y=0, width=75, height=25)
 
             tabControl.pack(expand=1, fill="both")
 
@@ -257,7 +310,15 @@ class Application(Tk):
     def subject_info_page(self, subject_name, group, week):
 
         self.clear_frame()
-        colors = ["red", "green", "yellow", "black", "gray", "yellow", "blue", 'brown']
+        colors = [
+            "red",
+            "green",
+            "yellow",
+            "black",
+            "gray",
+            "yellow",
+            "blue",
+            'brown']
         start_button = Button(text="Start", command=self.read_card)
 
         self.attendance = []
@@ -269,24 +330,48 @@ class Application(Tk):
         for subject in teacher.subjects_list:
             for student in subject.student_list:
                 groups.setdefault(student.cv_string, []).append(student.name)
-                attendance.setdefault(student.name, []).append(student.attendance)
+                attendance.setdefault(
+                    student.name, []).append(
+                    student.attendance)
 
         self.attendance = attendance
         self.groups = groups
 
         title_label = Label(self, text=subject_name, font=self.title_font)
-        back_button = Button(self, text="Back", command=lambda: [self.subjects_page(1), self.geometry("600x500"),
-                                                                 self.minsize(height=500, width=600)])
+        back_button = Button(
+            self,
+            text="Back",
+            command=lambda: [
+                self.subjects_page(1),
+                self.geometry("600x500"),
+                self.minsize(
+                    height=500,
+                    width=600)])
 
         self.popupMenu = Menu(self, tearoff=0)
-        self.popupMenu.add_command(label="red", command=lambda: self.change_attendance(subject_name, group, week, 0))
-        self.popupMenu.add_command(label="green", command=lambda: self.change_attendance(subject_name, group, week, 1))
-        self.popupMenu.add_command(label="yellow", command=lambda: self.change_attendance(subject_name, group, week, 2))
-        self.popupMenu.add_command(label="black", command=lambda: self.change_attendance(subject_name, group, week, 3))
-        self.popupMenu.add_command(label="gray", command=lambda: self.change_attendance(subject_name, group, week, 4))
+        self.popupMenu.add_command(
+            label="red", command=lambda: self.change_attendance(
+                subject_name, group, week, 0))
+        self.popupMenu.add_command(
+            label="green", command=lambda: self.change_attendance(
+                subject_name, group, week, 1))
+        self.popupMenu.add_command(
+            label="yellow", command=lambda: self.change_attendance(
+                subject_name, group, week, 2))
+        self.popupMenu.add_command(
+            label="black", command=lambda: self.change_attendance(
+                subject_name, group, week, 3))
+        self.popupMenu.add_command(
+            label="gray", command=lambda: self.change_attendance(
+                subject_name, group, week, 4))
 
         self.popupMenu2 = Menu(self, tearoff=0)
-        self.popupMenu2.add_command(label="Change group", command=lambda: self.change_group(group, subject_name, week))
+        self.popupMenu2.add_command(
+            label="Change group",
+            command=lambda: self.change_group(
+                group,
+                subject_name,
+                week))
 
         OPTIONS = []
         # OPTIONS.append("Skupina")
@@ -307,11 +392,21 @@ class Application(Tk):
         week_variable.set(week)
 
         week_option_menu = OptionMenu(self, week_variable, *week_options)
-        week_option_menu.place(relx=0.375, x=-150, rely=0.18, width=150, height=28)
+        week_option_menu.place(
+            relx=0.375,
+            x=-150,
+            rely=0.18,
+            width=150,
+            height=28)
 
         tmp, week_number = str(week_variable.get()).split(' ')
-        button = Button(self, text="Select",
-                        command=lambda: self.subject_info_page(subject_name, variable.get(), week_variable.get()))
+        button = Button(
+            self,
+            text="Select",
+            command=lambda: self.subject_info_page(
+                subject_name,
+                variable.get(),
+                week_variable.get()))
         button.place(relx=0.375, x=350, rely=0.18, width=150, height=25)
         bot = 0
         if group is not "Skupina":
@@ -320,46 +415,72 @@ class Application(Tk):
             for k in range(0, 12):
                 if (k == int(week_number) - 1):
                     t = k
-                    t = Label(self, text=str(k + 1), bg="#37d3ff", borderwidth=2, highlightthickness=2,
-                              highlightcolor="#37d3ff", highlightbackground="#37d3ff", )
-                    t.place(relx=0.4, x=+(k * 25), rely=0.3, y=-22, width=21, height=21)
+                    t = Label(
+                        self,
+                        text=str(
+                            k + 1),
+                        bg="#37d3ff",
+                        borderwidth=2,
+                        highlightthickness=2,
+                        highlightcolor="#37d3ff",
+                        highlightbackground="#37d3ff",
+                    )
+                    t.place(relx=0.4, x=+(k * 25), rely=0.3,
+                            y=-22, width=21, height=21)
                 else:
                     t = k
                     t = Label(self, text=str(k + 1))
-                    t.place(relx=0.4, x=+(k * 25), rely=0.3, y=-22, width=20, height=20)
+                    t.place(relx=0.4, x=+(k * 25), rely=0.3,
+                            y=-22, width=20, height=20)
 
             for name in self.groups[group]:
 
                 a = name
                 a = Label(self, text=name, anchor="w")
-                a.place(relx=0.375, x=-150, rely=0.3, y=+(i * 20), width=150, height=20)
+                a.place(relx=0.375, x=-150, rely=0.3,
+                        y=+(i * 20), width=150, height=20)
                 a.bind(self.right_click, self.popup_student)
                 a.bind("<Enter>", self.on_enter)
 
                 for j in range(0, 12):
                     if (j == int(week_number) - 1):
                         g = j
-                        g = Label(self, text=name + "_" + str(j), bg=colors[self.attendance[name][0][j]],
-                                  fg=colors[self.attendance[name][0][j]]
-                                  , borderwidth=2, highlightthickness=2, highlightcolor="#37d3ff",
-                                  highlightbackground="#37d3ff", )
+                        g = Label(self,
+                                  text=name + "_" + str(j),
+                                  bg=colors[self.attendance[name][0][j]],
+                                  fg=colors[self.attendance[name][0][j]],
+                                  borderwidth=2,
+                                  highlightthickness=2,
+                                  highlightcolor="#37d3ff",
+                                  highlightbackground="#37d3ff",
+                                  )
 
-                        g.place(relx=0.4, x=+(j * 25), rely=0.3, y=+(i * 20), width=21, height=21)
+                        g.place(relx=0.4, x=+(j * 25), rely=0.3,
+                                y=+(i * 20), width=21, height=21)
                         g.bind(self.right_click, self.popup)
-                        g.bind("<Button-1>", lambda event: self.left_click(event, subject_name, group, week))
+                        g.bind(
+                            "<Button-1>",
+                            lambda event: self.left_click(
+                                event,
+                                subject_name,
+                                group,
+                                week))
                         g.bind("<Enter>", self.on_enter)
                     else:
                         g = j
-                        g = Label(self, text=name + "_" + str(j), bg=colors[self.attendance[name][0][j]],
-                                  fg=colors[self.attendance[name][0][j]]
-                                  )
-                        g.place(relx=0.4, x=+(j * 25), rely=0.3, y=+ (i * 20), width=18, height=18)
+                        g = Label(self,
+                                  text=name + "_" + str(j),
+                                  bg=colors[self.attendance[name][0][j]],
+                                  fg=colors[self.attendance[name][0][j]])
+                        g.place(relx=0.4, x=+(j * 25), rely=0.3,
+                                y=+ (i * 20), width=18, height=18)
 
                 bot = len(self.groups[group])
                 i = i + 1
 
         title_label.place(relx=0.385, rely=0.08, width=300, height=25)
-        back_button.place(relx=0.375, x=85, rely=0.25, y=+(25 * (bot)), width=150, height=25)
+        back_button.place(relx=0.375, x=85, rely=0.25, y=+
+                          (25 * (bot)), width=150, height=25)
         start_button.place(x=0, y=0, width=75, height=25)
 
         self.minsize(height=(250 + (bot * 25)), width=1000)
@@ -385,8 +506,14 @@ class Application(Tk):
     def close_window(self, window):
         window.destroy()
 
-    #TODO Upravit to
-    def move_stundet_to_other_group(self, window, from_group, to_group, subject_name, week_selected):
+    # TODO Upravit to
+    def move_stundet_to_other_group(
+            self,
+            window,
+            from_group,
+            to_group,
+            subject_name,
+            week_selected):
 
         self.groups[to_group].append(self.selected)
         self.groups[from_group].remove(self.selected)
@@ -410,9 +537,15 @@ class Application(Tk):
         w = OptionMenu(toplevel, variable, *OPTIONS)
         w.place(x=5, y=5, width=150, height=25)
 
-        button = Button(toplevel, text="Select",
-                        command=lambda: self.move_stundet_to_other_group(toplevel, group, variable.get(), subject_name,
-                                                                         week_selected))
+        button = Button(
+            toplevel,
+            text="Select",
+            command=lambda: self.move_stundet_to_other_group(
+                toplevel,
+                group,
+                variable.get(),
+                subject_name,
+                week_selected))
         button.place(x=160, y=5, width=150, height=25)
 
     def change_attendance(self, subject_name, group, week_selected, a):
