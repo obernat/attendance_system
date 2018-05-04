f9a756df582c8255e36f82fa29a6dc1176948e99
Ondrik <ondrik.bernat@gmail.com>
2018-05-04 21:13:25 +0200

    sign_up

--- sign_up.py
+++ sign_up.py
@@ -8,7 +8,156 @@ import re
 if (len(sys.argv) == 1):
     print ("Please, specify your password")
     sys.exit()
-password = sys.argv[1]
+
+#mY
+if sys.argv[1] == "mY":
+    with open("/home/ondrej/mY.txt") as f:
+        password = f.read().strip()
+#if not mY
+else:
+    password = sys.argv[1]
+"""
+#Creating form sending to login
+payload = {
+"lang" : "sk",
+'destination': '/auth',
+#'credential_0': 'xbernato',
+'credential_0': 'xjokay',
+'credential_1': password,
+'credential_2': '86400',
+'login': 'Prihlásenie'
+}
+
+#Creating session. Session object persist cookies, uses connection pooling
+s = requests.Session()
+
+#Creating post with given payload to log in
+r = s.post("https://test.is.stuba.sk/auth/?lang=sk", data=payload)
+
+#Creating get to teacher page
+r = s.get("https://test.is.stuba.sk/auth/ucitel/?_m=195;lang=sk") #what is _m?
+#_--------------------------------------------------
+
+
+
+import sqlite3
+import http.cookiejar as cookielib
+
+def get_cookies(cj, ff_cookies):
+    con = sqlite3.connect(ff_cookies)
+    cur = con.cursor()
+    cur.execute("SELECT host, path, isSecure, expiry, name, value FROM moz_cookies")
+    for item in cur.fetchall():
+        if item[0].find("stuba.sk") == -1:
+            continue
+        c = cookielib.Cookie(0, item[4], item[5],
+            None, False,
+            item[0], item[0].startswith('.'), item[0].startswith('.'),
+            item[1], False,
+            item[2],
+            item[3], item[3]=="",
+            None, None, {})
+        print (c)
+        cj.set_cookie(c)
+
+
+cj = cookielib.CookieJar()
+ff_cookies = sys.argv[2]
+get_cookies(cj, ff_cookies)
+print(cj)
+s = requests.Session()
+s.cookies = cj
+print(s.cookies)
+sys.exit()
+r = s.get("https://test.is.stuba.sk/auth/ucitel/?_m=195;lang=sk") #what is _m?
+print (r.text)
+"""
+
+
+url = 'https://test.is.stuba.sk/auth/ucitel/?_m=195;lang=sk'
+#cookie_file = sys.argv[2]
+
+
+
+def get_cookie_jar(filename):
+    """
+    Protocol implementation for handling gsocmentors.com transactions
+    Author: Noah Fontes nfontes AT cynigram DOT com
+    License: MIT
+    Original: http://blog.mithis.net/archives/python/90-firefox3-cookies-in-python
+
+    Ported to Python 3 by Dotan Cohen
+    """
+
+    from io import StringIO
+    import http.cookiejar
+    import sqlite3
+
+    con = sqlite3.connect(filename)
+    cur = con.cursor()
+    cur.execute("SELECT host, path, isSecure, expiry, name, value FROM moz_cookies")
+
+    ftstr = ["FALSE","TRUE"]
+
+    s = StringIO()
+    s.write("""\
+# Netscape HTTP Cookie File
+# http://www.netscape.com/newsref/std/cookie_spec.html
+# This is a generated file!  Do not edit.
+""")
+
+    for item in cur.fetchall():
+        s.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (
+            item[0], ftstr[item[0].startswith('.')], item[1],
+            ftstr[item[2]], item[3], item[4], item[5]))
+
+    s.seek(0)
+    cookie_jar = http.cookiejar.MozillaCookieJar()
+    cookie_jar._really_load(s, '', True, True)
+
+    return cookie_jar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("asd")
+sys.exit()
+
+
 
 #Creating form sending to login
 payload = {
@@ -29,6 +178,58 @@ r = s.post("https://test.is.stuba.sk/auth/?lang=sk", data=payload)
 
 #Creating get to teacher page
 r = s.get("https://test.is.stuba.sk/auth/ucitel/?_m=195;lang=sk") #what is _m?
+print(s.cookies)
+from requests.utils import dict_from_cookiejar
+cookies = dict_from_cookiejar(s.cookies) # s is your session object
+
+for key, value in cookies.items():
+    print("1:", key, value)
+
+cj = get_cookie_jar(cookie_file)
+cookies = dict_from_cookiejar(cj) # s is your session object
+for key, value in cookies.items():
+    print("2:", key, value)
+
+
+
+sys.exit()
+#s = requests.Session()
+#r = s.get("https://test.is.stuba.sk/") #what is _m?
+s.cookies = cj
+r = requests.get("https://test.is.stuba.sk/auth/student/moje_studium.pl?_m=3110", cookies=cj) #what is _m?
+print (r.text)
+
+
+
+
+
+
+
+sys.exit()
+
+
+
+
+
+
+
+
+
+
+#_--------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -36,7 +237,6 @@ r = s.get("https://test.is.stuba.sk/auth/ucitel/?_m=195;lang=sk") #what is _m?
 from selenium import webdriver
 
 profile = webdriver.FirefoxProfile('/home/ondrej/.mozilla/firefox/azo5kg9b.default')
-
 browser = webdriver.Firefox(profile)
 
 
@@ -50,10 +250,116 @@ for key, value in cookies.items():
 
 browser.get("https://test.is.stuba.sk/auth/ucitel/?_m=195;lang=sk")
 
+browser.close()
+sys.exit()
+
+from threading import Thread
+from time import sleep
+import pyautogui
+
+def threaded_write_pin(browser):
+    print (browser.get_cookies())
+    pin = "2144"
+    sleep(3)
+    pyautogui.keyDown('shift')  #SVK layout
+    pyautogui.typewrite(pin, interval=0.25)
+    pyautogui.keyUp('shift')    #SVK layout
+
+    pyautogui.press('enter')
+    sleep(3)
+    pyautogui.press('enter')
+    sleep(3)
+    print("test")
+    url = browser.command_executor._url       #"http://127.0.0.1:60622/hub"
+    session_id = browser.session_id            #'4e167f26-dc1d-4f51-a207-f761eaf73c31'
+    browser.quit()
+    print (url, session_id)
+    return url, session_id
+
+
+print("0")
+#thread = Thread(target=threaded_write_pin, args=(browser, ))
+#browser.find_element_by_id("loginButton").click()
+from selenium.webdriver.common.keys import Keys
+#browser.set_page_load_timeout(4)
+browser.find_element_by_id("loginButton").send_keys(Keys.ENTER)
+
+print("1")
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#thread.start()
+#wait = WebDriverWait(browser, 10)
+#table = wait.until(EC.presence_of_element_located((By.ID, 'tmtab_1')))
+print("2")
+url, session_id = threaded_write_pin(browser)
+#thread.join()
 
 
 
-browser.find_element_by_id("loginButton").click()
+
+
+
+
+sys.exit()
+
+
+
+
+profile = webdriver.FirefoxProfile('/home/ondrej/.mozilla/firefox/azo5kg9b.default')
+driver = webdriver.Firefox(profile)
+
+
+
+driver.get("https://centrum.sk")
+print("test3")
+print (driver.get_cookies())
+print("3")
+sys.exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+def create_driver_session(session_id, executor_url):
+    from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
+
+    # Save the original function, so we can revert our patch
+    org_command_execute = RemoteWebDriver.execute
+
+    def new_command_execute(self, command, params=None):
+        if command == "newSession":
+            # Mock the response
+            return {'success': 0, 'value': None, 'sessionId': session_id}
+        else:
+            return org_command_execute(self, command, params)
+
+    # Patch the function before creating the driver object
+    RemoteWebDriver.execute = new_command_execute
+
+    new_driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
+    new_driver.session_id = session_id
+
+    # Replace the patched function with original function
+    RemoteWebDriver.execute = org_command_execute
+
+    return new_driver
+
+#driver = create_driver_session(session_id, url)
+
+
+
+#driver = webdriver.Remote(command_executor=url,desired_capabilities=firefox_capabilities)
+#driver.session_id = session_id
 
 
 
@@ -70,6 +376,24 @@ browser.find_element_by_id("loginButton").click()
 
 
 
+
+
+
+
+
+
+
+#print (browser.get_cookies())
+
+sys.exit()
+############################################################################
+print("cookie")
+print (browser.get_cookies())
+sys.exit()
+for cookie in browser.get_cookies():
+    print (cookie)
+    #c = {cookie['name']: cookie['value']}
+    #s.cookies.update(c)
 
 
 sys.exit() #TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
